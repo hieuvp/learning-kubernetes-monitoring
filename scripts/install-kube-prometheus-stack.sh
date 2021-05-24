76cccacfb8998fa7a8ba8ca576d8cd158ba65254
Harrison (Hieu) Van <Hieu_Van@mckinsey.com>
2021-05-24 19:29:46 +0700

    install-kube-prometheus-stack

--- scripts/install-kube-prometheus-stack.sh
+++ scripts/install-kube-prometheus-stack.sh
@@ -7,7 +7,9 @@ minikube start
 
 kubectl create namespace kube-prometheus-stack
 
-helm install kube-prometheus-stack ./kube-prometheus-stack --namespace=kube-prometheus-stack
+helm install kube-prometheus-stack ./kube-prometheus-stack \
+  --namespace=kube-prometheus-stack \
+  --set-string namespaceOverride=kube-prometheus-stack
 
 # kubectl port-forward --namespace=kube-prometheus-stack service/kube-prometheus-stack-grafana 3000:80
 # http://127.0.0.1:3000/
